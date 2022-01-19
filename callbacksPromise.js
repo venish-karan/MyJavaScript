6949f3c8150f3943a63242961ebcc95df11500ff
Venish P <sec19ec070@sairamtap.edu.in>
2022-01-20 00:06:09 +0530

    callback and promises in javascript

---
+++ callbacksPromise.js
@@ -0,0 +1,29 @@
+// scenario
+
+function register() {
+  console.log("register end");
+}
+
+function sendEmail() {
+  console.log("Email end");
+}
+
+function login() {
+  console.log("login end");
+}
+
+function getUserData() {
+  console.log("get data end");
+}
+
+function displayUserData() {
+  console.log("display data end");
+}
+
+register();
+sendEmail();
+login();
+getUserData();
+displayUserData();
+
+console.log("other application work");
