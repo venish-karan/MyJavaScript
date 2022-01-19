7537e059c5c8e508d2cd724bc7743d68de01adf6
Venish P <sec19ec070@sairamtap.edu.in>
2022-01-19 19:07:28 +0530

    functions parameters

---
+++ function.js
@@ -0,0 +1,21 @@
+// Functions
+
+function login(username, password) {
+  // code here
+  //   const name = window.prompt();
+  //   console.log(password); //undefined
+  console.log(`${username}, Logged in successfully`);
+}
+
+// login("venish");
+// login("john");
+
+function upperCase(str) {
+  //   str.toUpperCase();
+  //   console.log(str.toUpperCase());
+  return str.toUpperCase();
+}
+
+const upperCaseVal = upperCase("venish"); // by default function returns undefined
+
+// console.log(upperCaseVal);
