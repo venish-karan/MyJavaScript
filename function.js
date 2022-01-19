fd9ff99edab80a03332f8cb37089b3c6c6936821
Venish P <sec19ec070@sairamtap.edu.in>
2022-01-19 19:33:41 +0530

    functions parameters

--- function.js
+++ function.js
@@ -1,11 +1,11 @@
 // Functions
 
-function login(username, password) {
-  // code here
-  //   const name = window.prompt();
-  //   console.log(password); //undefined
-  console.log(`${username}, Logged in successfully`);
-}
+// function login(username, password) {
+//   // code here
+//   //   const name = window.prompt();
+//   //   console.log(password); //undefined
+//   console.log(`${username}, Logged in successfully`);
+// }
 
 // login("venish");
 // login("john");
@@ -19,3 +19,40 @@ function upperCase(str) {
 const upperCaseVal = upperCase("venish"); // by default function returns undefined
 
 // console.log(upperCaseVal);
+
+// default parameters
+function calculateArea(width, height = 1) {
+  // similar to height = undefined ? 1 : height;
+  const area = width * height;
+  return area;
+}
+
+const area = calculateArea(30); // not passing argument returns NaN => not a number
+
+// variable scope
+
+function download() {
+  const fileName = "xyz.abc";
+}
+
+download();
+
+// anonymous function
+// function expression
+const login = function () {
+  console.log("Logged in");
+};
+
+login();
+
+// callback function
+
+const formatText = function (text, formatcb) {
+  // typeof return the datatype of the variable
+  return typeof formatcb === "function" ? formatcb(text) : text.toUpperCase();
+};
+
+const result = formatText("hello", function (text) {
+  return text.charAt(0).toUpperCase() + text.slice(1);
+});
+console.log(result);
