719bb1508473c0c26d5e315ee08bb54239963cc2
Venish P <sec19ec070@sairamtap.edu.in>
2022-01-20 02:15:05 +0530

    DOM manipulation in javascript

---
+++ DOMmanipulation.js
@@ -0,0 +1,33 @@
+// DOM manipulation Document object model
+
+// 1. getElementById
+
+// const heading = document.getElementById("heading");
+// //.innerText = "Yes it is");
+// console.log(heading);
+
+// getElementByTagName
+
+// const heading = document.getElementsByTagName('h1');
+
+// console.log(heading[0]);
+
+// getElementsByClassName
+
+// querySelector
+
+const heading = document.querySelector("#heading");
+
+// traverse node
+
+// parent node property
+
+const parent = heading.parentNode;
+
+// childnode property
+
+const parent = document.querySelector(".parent");
+
+console.log(parent.childNodes);
+
+// nextElementSibling
