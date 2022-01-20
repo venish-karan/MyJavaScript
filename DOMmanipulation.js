737cc67dd600185f90f8af48c75826719bd5c9b2
Venish P <sec19ec070@sairamtap.edu.in>
2022-01-20 12:09:42 +0530

    DOM manipulation

--- DOMmanipulation.js
+++ DOMmanipulation.js
@@ -16,18 +16,62 @@
 
 // querySelector
 
-const heading = document.querySelector("#heading");
+// const heading = document.querySelector("#heading");
 
 // traverse node
 
 // parent node property
-
-const parent = heading.parentNode;
+// const parent = heading.parentNode;
 
 // childnode property
 
-const parent = document.querySelector(".parent");
+// const parent = document.querySelector(".parent");
 
-console.log(parent.childNodes);
+// console.log(parent.childNodes);
 
 // nextElementSibling
+
+// const heading = document.querySelector("#heading");
+
+// console.log(heading.nextElementSibling);
+
+// it will get first h3 element
+// const subHeading = document.querySelector("h3");
+
+// console.log(subHeading.previousElementSibling);
+
+// Manipulation
+
+// const heading = document.querySelector(".heading");
+
+// heading.innerHTML = "web dev is awesome";
+
+// heading.style.color = "red";
+
+// heading.style.fontSize = "100px";
+
+// heading.classList.add("title"); // remove
+
+// create elements
+
+// const heading = document.createElement("h1");
+// heading.innerText = "This is newly created node";
+// heading.classList.add("title");
+
+// const subHeading = document.createElement("h1");
+// subHeading.innerText = "This is newly created node";
+// subHeading.classList.add("nextHeading");
+
+// const parent = document.querySelector(".parent");
+// parent.appendChild(heading);
+
+// heading.insertAdjacentElement("beforebegin", subHeading); // afterend
+
+// Events
+const button = document.querySelector("#btn");
+const heading = document.querySelector("#heading");
+
+btn.addEventListener("click", (event) => {
+  console.log(event);
+  heading.style.color = "red";
+});
